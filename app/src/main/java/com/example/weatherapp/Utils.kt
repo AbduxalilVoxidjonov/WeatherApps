7f028e11890b2bcde8e3b10bc086ae75549faffa
Abduxalil Voxidjonov <99504511+AbduxalilVoxidjonov@users.noreply.github.com>
2023-11-01 04:28:07 -0700

    commit

---
+++ app/src/main/java/com/example/weatherapp/Utils.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapp
+
+class Utils {
+    companion object{
+
+        var BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
+        var API_KEY  : String = "bc19f330b3f83b6a18b21b4bb2388837"
+         const val PERMISSION_REQUEST_CODE = 123
+
+         const val LOCATION_PERMISSION_REQUEST_CODE = 1001
+
+
+
+
+
+
+    }
+}
